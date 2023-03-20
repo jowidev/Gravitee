4be47ff7b277d98bff50a34b6812f931e3799ff0
Jowi <xoanmanuelaltaleffpais@gmail.com>
2023-03-20 15:08:49 -0300

    arregla timer

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var props= preload("res://mob.tscn")
 func _ready():
-	pass # Replace with function body.
+	$Timer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Timer_timeout():
+	var mob = props.instance()
+	add_child(mob)
+	print("1")

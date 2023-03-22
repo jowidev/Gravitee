7faa2911ee7420e8f1907d53004cd55bc8e91bd6
Jowi <xoanmanuelaltaleffpais@gmail.com>
2023-03-21 23:42:42 -0300

    finaliza collider

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,6 @@ var timeschange = 0
 
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- Scripts/mob.gd
+++ Scripts/mob.gd
@@ -5,7 +5,7 @@ var max_speed = 250
  
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_to_group("Mob")
 
 func move():
 	self.position.x-=min_speed
@@ -16,3 +16,4 @@ func _physics_process(_delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+	print("destroyed")

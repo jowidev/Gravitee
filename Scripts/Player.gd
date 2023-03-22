496966dc7f5adb4ff71edb075e44310d68f4c45e
Jowi <xoanmanuelaltaleffpais@gmail.com>
2023-03-21 23:29:17 -0300

    avanza en spawner

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 var grav = 25
 var speed = Vector2.ZERO #0,0
 var timeschange = 0
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,3 +23,7 @@ func _physics_process(_delta):
 	get_input()
 	speed.y += grav
 	speed = move_and_slide(speed)
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Mob"):
+		queue_free()
